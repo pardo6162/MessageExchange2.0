0f30366cea4f13cf736b10db0b66c47b86c094d0
Andres Felipe Pardo Mesa <a.pardo6162@gmail.com>
2018-10-15 21:53:48 -0500

    Second part implemented message send

--- messageExchange.sol
+++ messageExchange.sol
@@ -5,7 +5,7 @@ contract MessageExchange {
     
     struct User{
         bytes32 name;
-        uint unreadedmsg;
+        uint msgcount;
         bool isMember;
     }
             
@@ -21,20 +21,36 @@ contract MessageExchange {
         users[msg.sender]= User(name,0,true);
     }
     
-    function addFriend(address requestReceiver){
+    function addFriend(address requestReceiver) public onlyMember{
+        require (requestReceiver != msg.sender, "Not add itself like friend");
         relations[msg.sender][requestReceiver]=Relationship.Pending;
         relations[requestReceiver][msg.sender]=Relationship.Pending;
     }
     
-    function acceptFriend(address requestSender){
+    function acceptFriend(address requestSender) public onlyMember{
+        require (relations[msg.sender][requestSender]==Relationship.Pending);
+        require (relations[requestSender][msg.sender]==Relationship.Pending);
         relations[msg.sender][requestSender]=Relationship.Friend;
         relations[requestSender][msg.sender]=Relationship.Friend;
     }
     
-    function rejectRequest(address requestSender){
+    function rejectRequest(address requestSender) public onlyMember{
+        require (relations[msg.sender][requestSender]==Relationship.Pending);
+        require (relations[requestSender][msg.sender]==Relationship.Pending);
         relations[msg.sender][requestSender]=Relationship.Unknown;
         relations[requestSender][msg.sender]=Relationship.Friend;
     }
-
     
+    function sendMessage(bytes32 message , address receiver) public onlyMember{
+        require(receiver!= msg.sender);
+        require (relations[msg.sender][receiver]==Relationship.Friend);
+        require (relations[receiver][msg.sender]==Relationship.Friend);
+        users[receiver].msgcount+=1;
+        inbox[receiver][(users[receiver].msgcount)-1]=message;
+    }
+
+    modifier onlyMember(){
+         require(users[msg.sender].isMember==true);
+         _;
+     }
 }
