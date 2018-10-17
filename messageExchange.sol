bc16e9a1dd1e9914328b79f0b7f2e5b944a4e33f
Andres Felipe Pardo Mesa <a.pardo6162@gmail.com>
2018-10-16 21:42:42 -0500

    Events added

--- messageExchange.sol
+++ messageExchange.sol
@@ -1,6 +1,12 @@
 pragma solidity ^0.4.22;
 contract MessageExchange {
     
+    event userAdded(bytes32 name,address userAddress);
+    event requestSent(address sender,address receiver);
+    event friendAdded(address user1,address user2 );
+    event userRejected(address user1,address user2 );
+    event messageSent(bytes32 message, address sender);
+        
     enum Relationship {Unknown,Pending,Friend}
     
     struct User{
@@ -19,12 +25,14 @@ contract MessageExchange {
     
     function addUser(bytes32 name) public {
         users[msg.sender]= User(name,0,true);
+        emit userAdded(name,msg.sender);
     }
     
     function addFriend(address requestReceiver) public onlyMember{
         require (requestReceiver != msg.sender, "Not add itself like friend");
         relations[msg.sender][requestReceiver]=Relationship.Pending;
         relations[requestReceiver][msg.sender]=Relationship.Pending;
+        emit requestSent(msg.sender,requestReceiver);
     }
     
     function acceptFriend(address requestSender) public onlyMember{
@@ -32,6 +40,7 @@ contract MessageExchange {
         require (relations[requestSender][msg.sender]==Relationship.Pending);
         relations[msg.sender][requestSender]=Relationship.Friend;
         relations[requestSender][msg.sender]=Relationship.Friend;
+        emit friendAdded(msg.sender,requestSender);
     }
     
     function rejectRequest(address requestSender) public onlyMember{
@@ -39,6 +48,7 @@ contract MessageExchange {
         require (relations[requestSender][msg.sender]==Relationship.Pending);
         relations[msg.sender][requestSender]=Relationship.Unknown;
         relations[requestSender][msg.sender]=Relationship.Friend;
+        emit userRejected(msg.sender,requestSender); 
     }
     
     function sendMessage(bytes32 message , address receiver) public onlyMember{
@@ -47,6 +57,7 @@ contract MessageExchange {
         require (relations[receiver][msg.sender]==Relationship.Friend);
         users[receiver].msgcount+=1;
         inbox[receiver][(users[receiver].msgcount)-1]=message;
+        emit messageSent(message,receiver);
     }
 
     modifier onlyMember(){
